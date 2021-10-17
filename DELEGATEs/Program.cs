ff4f0f570b0f7909701c55b7ef5ef7371afb90f9
shadeamin <mdshadeamin@gmail.com>
2021-10-17 21:14:55 +0600

    done

---
+++ DELEGATEs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DELEGATEs
+{
+    class Program
+    {
+        public delegate void MyWorkFor(string message);
+        static void Main(string[] args)
+        {
+            MyWorkFor a = Print;
+            a += Print2;//a=a+Print2
+            a += Print3;
+            a("aaa");
+            Console.ReadLine();
+        }
+       
+
+        public static void Print(string item)
+        {
+            Console.WriteLine(item+"I am from print1");
+        }
+        public static void Print2(string item2)
+        {
+            Console.WriteLine(item2 + "I am from print2");
+        }
+        public static void Print3(string item3)
+        {
+            Console.WriteLine(item3 + "I am from print3");
+        }
+    }
+}
---
+++ EventExamples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EventExamples
+{
+    class Program
+    {
+        public delegate void MyDelelgate();
+        public static event MyDelelgate notification;
+        static void Main(string[] args)
+        {
+            notification += Program_notification;
+            notification += Program_notification1;
+            notification += Program_notification2;
+            notification();
+        }
+
+        private static void Program_notification2()
+        {
+            Console.WriteLine("Savar");
+        }
+
+        private static void Program_notification1()
+        {
+            Console.WriteLine("Mirpur");
+        }
+
+        private static void Program_notification()
+        {
+            Console.WriteLine("Savareeeeeeee");
+        }
+    }
+}
